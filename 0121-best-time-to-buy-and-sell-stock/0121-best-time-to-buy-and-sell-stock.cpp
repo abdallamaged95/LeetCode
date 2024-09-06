3323db73d0995046617ee1ee6cc2966abd676e89
abdalla maged <100102723+abdallamaged95@users.noreply.github.com>
2024-09-06 16:56:46 +0300

    Time: 47 ms (99.51%), Space: 96.4 MB (6.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
         int sell = 0, profit = 0;
         for (int i = 1; i < prices.size(); i++) {
             profit = max(profit, prices[i] - prices[sell]);
