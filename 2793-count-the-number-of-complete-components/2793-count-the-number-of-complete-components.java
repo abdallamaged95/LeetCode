abfba7b1f97dd61fb87a00196b7a3f82ffcfd9b2
abdalla maged <100102723+abdallamaged95@users.noreply.github.com>
2024-09-13 14:56:24 +0300

    Time: 12 ms (50.56%), Space: 44.9 MB (75.92%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+
+
+
+class Solution {
+    List<Integer>[] graph;
+    char[] visited;
+    public int countCompleteComponents(int n, int[][] edges) {
+        visited = new char[n];
+        Arrays.fill(visited, 'W');
+
+        graph = new List[n];
+        // init graph
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < edges.length; i++) {
+            graph[edges[i][0]].add(edges[i][1]);
+            graph[edges[i][1]].add(edges[i][0]);
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+            if (visited[i] == 'W') {
+                if (BFS(i))
+                    cnt++;
+            }
+
+        return cnt;
+    }
+
+    public boolean BFS(int vrtx) {
+        int edgesCnt = 0, nodesCnt = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(vrtx);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            visited[curr] = 'B';
+            nodesCnt++;
+            for (int i = 0; i < graph[curr].size(); i++) {
+                if (visited[graph[curr].get(i)] == 'W') {
+                    q.add(graph[curr].get(i));
+                    visited[graph[curr].get(i)] = 'G';
+                    edgesCnt++;
+                }
+                else if (visited[graph[curr].get(i)] == 'G')
+                    edgesCnt++;
+            }
+        }
+        if (edgesCnt == ((nodesCnt-1) * nodesCnt) / 2)
+            return true;
+        else if (nodesCnt == 2 && edgesCnt == 1)
+            return true;
+        else if (nodesCnt == 1 && edgesCnt == 0)
+            return true;
+        return false;
+    }
+}
