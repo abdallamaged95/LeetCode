5f63437ce8a8d06ad7ee11c74c0399b85ed699c9
abdalla maged <100102723+abdallamaged95@users.noreply.github.com>
2024-09-13 15:04:45 +0300

    Time: 12 ms (50.56%), Space: 45.2 MB (37.24%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+
+
+
 class Solution {
     List<Integer>[] graph;
     char[] visited;
@@ -28,7 +31,6 @@ class Solution {
     }
 
     public boolean BFS(int vrtx) {
-        List<Integer> nodes = new ArrayList<>();
         int edgesCnt = 0, nodesCnt = 0;
         Queue<Integer> q = new LinkedList<>();
         q.add(vrtx);
@@ -36,7 +38,6 @@ class Solution {
             int curr = q.remove();
             visited[curr] = 'B';
             nodesCnt++;
-            nodes.add(curr);
             for (int i = 0; i < graph[curr].size(); i++) {
                 if (visited[graph[curr].get(i)] == 'W') {
                     q.add(graph[curr].get(i));
@@ -47,22 +48,12 @@ class Solution {
                     edgesCnt++;
             }
         }
-
-//        if (edgesCnt == ((nodesCnt-1) * nodesCnt) / 2)
-//            return true;
-//        else if (nodesCnt == 2 && edgesCnt == 1)
-//            return true;
-//        else if (nodesCnt == 1 && edgesCnt == 0)
-//            return true;
-        boolean flag = true;
-        for (int i = 0; i < nodes.size(); i++) {
-            for (int j = 0; j < nodes.size() && j != i; j++) {
-                if (!graph[nodes.get(i)].contains(nodes.get(j)))
-                    flag = false;
-            }
-        }
-
-        return flag;
+        if (edgesCnt == ((nodesCnt-1) * nodesCnt) / 2)
+            return true;
+        else if (nodesCnt == 2 && edgesCnt == 1)
+            return true;
+        else if (nodesCnt == 1 && edgesCnt == 0)
+            return true;
+        return false;
     }
-
 }
