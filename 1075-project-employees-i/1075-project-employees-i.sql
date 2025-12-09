bbc054d99f71e60e2feeff51b5433e0b18807d3e
abdalla maged <abdalla.maged95@gmail.com>
2025-12-09 19:52:58 +0200

    Time: 509 ms (37.94%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,4 +1,4 @@
-select project_id, round(sum(experience_years) / count(p.employee_id), 2) as average_years
+select project_id, round(avg(experience_years), 2) as average_years
 from project p
 join employee e on e.employee_id = p.employee_id
 group by project_id;
