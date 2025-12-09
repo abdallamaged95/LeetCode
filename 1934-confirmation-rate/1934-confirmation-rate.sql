1b74434a436edfb03f120f8d9253e6d192436533
abdalla maged <abdalla.maged95@gmail.com>
2025-12-09 20:12:43 +0200

    Time: 811 ms (8.79%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,24 +1,16 @@
--- # Write your MySQL query statement below
 
 
--- select user_id, count(*) 
--- from (
---     select user_id, action
---     from signups s
---     left join confirmations c using (user_id)
--- ) e 
--- group by user_id;
 
-select outer_s.user_id, ROUND((
-    select count(c.user_id)
-    from signups s
-    left join confirmations c on s.user_id = c.user_id and action = 'confirmed'
-    where s.user_id = outer_s.user_id
-)
-/ (
+select user_id, round(
+    (
+    select count(action)
+    from signups
+    left join confirmations c on c.user_id = s.user_id and action = 'confirmed'
+    where signups.user_id = s.user_id
+    ) / (
     select count(*)
-    from signups s
-    left join confirmations c on c.user_id = s.user_id
-    where s.user_id = outer_s.user_id
-), 2) as confirmation_rate
-from signups outer_s;
+    from signups
+    left join confirmations c using (user_id)
+    where signups.user_id = s.user_id
+    ), 2) as confirmation_rate
+from signups s;
