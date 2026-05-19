1b6f4336f5fa407aa76a8615f7f5f2ec2736be7b
abdalla maged <abdalla.maged95@gmail.com>
2026-05-19 18:13:23 +0300

    Time: 2 ms (52.42%), Space: 52.9 MB (25.94%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int stepsA = 1, stepsB = 1;
+        ListNode nodeA = headA, nodeB = headB;
+        while (nodeA.next != null || nodeB.next != null) {
+            if (nodeA == nodeB && stepsA == stepsB)
+                return nodeA;
+            if (nodeA.next != null) {
+                nodeA = nodeA.next;
+                stepsA += 1;
+            }
+            if (nodeB.next != null) {
+                nodeB = nodeB.next;
+                stepsB += 1;
+            }
+        }
+        nodeA = headA;
+        nodeB = headB;
+        while (stepsB > stepsA) {
+            nodeB = nodeB.next;
+            stepsB -= 1;
+        }
+        while (stepsA > stepsB) {
+            nodeA = nodeA.next;
+            stepsA -= 1;
+        }
+        while (nodeA != null && nodeB != null && nodeA != nodeB) {
+            nodeB = nodeB.next;
+            nodeA = nodeA.next;
+        }
+        if (nodeA == null || nodeB == null)
+            return null;
+        return nodeB;
+    }
+}
