75636b9abbe5bc1f7965a9df56d9baa8186a22e8
abdalla maged <abdalla.maged95@gmail.com>
2026-05-23 11:24:55 +0300

    Time: 0 ms (100%), Space: 44.3 MB (55.64%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,35 +8,26 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+public class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null)
-            return list2;
-        else if (list2 == null)
-            return list1;
-
-        ListNode n1 = list1, n2 = list2;
-        while (n1 != null && n2 != null) {
-            if (n2.val <= n1.val) {
-                while (n2.next != null && n2.next.val <= n1.val)
-                    n2 = n2.next;
-                ListNode tmp = n2;
-                n2 = n2.next;
-                tmp.next = n1;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                dummy.next = list1;
+                list1 = list1.next;
             }
             else {
-                while (n1.next != null && n1.next.val <= n2.val)
-                    n1 = n1.next;
-                ListNode tmp = n1;
-                n1 = n1.next;
-                tmp.next = n2;
+                dummy.next = list2;
+                list2 = list2.next;
             }
+            dummy = dummy.next;
         }
-        if (list1.val < list2.val) {
-            ListNode tmp = list1;
-            list1 = list2;
-            list2 = tmp;
-        }
-        return list2;
+        if (list1 != null)
+            dummy.next = list1;
+        if (list2 != null)
+            dummy.next = list2;
+        return head.next;
     }
+
 }
